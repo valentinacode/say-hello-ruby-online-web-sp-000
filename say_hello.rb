e6696e98e72daed263beddfddc8769cf53f8ac97
Valentina E <tiyahelfrih@gmail.com>
2020-07-04 11:22:07 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 
 say_hello("Kent Beck!")
 say_hello
+=======
+def say_hello(name)
+  puts "Hello, #{name}"
+end
+
+say_hello("Gabriela")
+>>>>>>> ac2defd407dd5f860796a1251051b60233ec1a35
